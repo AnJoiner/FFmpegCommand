737e5293e164ebcb55849638d56dd9e4c1e67353
AnJoiner <anjoiner@163.com>
2019-12-18 14:09:23 +0800

    更新ffmpeg

--- ffmpeg/src/main/java/com/coder/ffmpeg/jni/FFmpegCommand.java
+++ ffmpeg/src/main/java/com/coder/ffmpeg/jni/FFmpegCommand.java
@@ -1,5 +1,7 @@
 package com.coder.ffmpeg.jni;
 
+import android.util.Log;
+
 import com.coder.ffmpeg.call.ICallBack;
 
 import org.reactivestreams.Subscriber;
@@ -18,12 +20,26 @@ import io.reactivex.schedulers.Schedulers;
  */
 public class FFmpegCommand {
 
+    /**
+     * 同步
+     * @param cmd　ffmpeg 命令
+     */
+    public static void runSync(final String[] cmd){
+        FFmpegCmd.runCmd(cmd);
+    }
+
+    /**
+     * 异步
+     * @param cmd　ffmpeg 命令
+     * @param callBack　异步回调
+     */
     public static void runAsync(final String[] cmd, final ICallBack callBack) {
 
         Flowable.create(new FlowableOnSubscribe<Integer>() {
             @Override
             public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
-                FFmpegCmd.runCmd(cmd);
+                int code = FFmpegCmd.runCmd(cmd);
+                Log.d("FFmpegCommand","code:"+code);
                 emitter.onComplete();
             }
         }, BackpressureStrategy.BUFFER)
