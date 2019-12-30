4b6edb395917cbd6a970fd41d4a300af9b47f240
AnJoiner <anjoiner@163.com>
2019-12-30 21:15:57 +0800

    更新ffmpeg-command 1.0.9

--- app/src/main/java/com/coder/ffmpegtest/ui/FFmpegCommandActivity.java
+++ app/src/main/java/com/coder/ffmpegtest/ui/FFmpegCommandActivity.java
@@ -118,58 +118,58 @@ public class FFmpegCommandActivity extends AppCompatActivity {
                         concatVideo();
                         break;
                     case 6:
-                        reduceAudio();
-                        break;
-                    case 7:
                         extractAudio();
                         break;
-                    case 8:
+                    case 7:
                         extractVideo();
                         break;
-                    case 9:
+                    case 8:
                         mixAudioVideo();
                         break;
-                    case 10:
+                    case 9:
                         screenShot();
                         break;
-                    case 11:
+                    case 10:
                         video2Image();
                         break;
-                    case 12:
+                    case 11:
                         video2Gif();
                         break;
-                    case 13:
+                    case 12:
                         addWaterMark();
                         break;
-                    case 14:
+                    case 13:
                         image2Video();
                         break;
-                    case 15:
+                    case 14:
                         decodeAudio();
                         break;
-                    case 16:
+                    case 15:
                         encodeAudio();
                         break;
-                    case 17:
+                    case 16:
                         multiVideo();
                         break;
-                    case 18:
+                    case 17:
                         reverseVideo();
                         break;
-                    case 19:
+                    case 18:
                         picInPic();
                         break;
-                    case 20:
+                    case 19:
                         mixAudio();
                         break;
-                    case 21:
+                    case 20:
                         videoDoubleDown();
                         break;
-                    case 22:
+                    case 21:
                         videoSpeed2();
                         break;
-                    case 23:
+                    case 22:
                         denoiseVideo();
+                        break;
+                    case 23:
+                        reduceAudio();
                         break;
                     case 24:
                         video2YUV();
