554744faab2198fb3230bee0c212f97cd7cde10c
anjoiner <anjoiner@163.com>
2021-02-01 13:21:57 +0800

    优化

--- app/src/main/java/com/coder/ffmpegtest/service/FFmpegCommandService.kt
+++ app/src/main/java/com/coder/ffmpegtest/service/FFmpegCommandService.kt
@@ -1,9 +1,17 @@
 package com.coder.ffmpegtest.service
 
+import android.app.IntentService
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
+import com.coder.ffmpeg.annotation.MediaAttribute
+import com.coder.ffmpeg.call.CommonCallBack
 import com.coder.ffmpeg.jni.FFmpegCommand
+import com.coder.ffmpegtest.utils.FileUtils
+import com.coder.ffmpegtest.utils.ToastUtils
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.io.File
 import java.util.*
 
@@ -11,18 +19,56 @@ import java.util.*
  * @author: AnJoiner
  * @datetime: 20-6-28
  */
-class FFmpegCommandService : Service() {
+class FFmpegCommandService : IntentService("") {
+    companion object{
+        val TAG = "FFmpegCommandService"
+    }
     override fun onBind(intent: Intent): IBinder? {
         return null
     }
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.e(TAG,"onStartCommand")
+        FileUtils.copy2Memory(this, "test.mp4")
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        Log.e(TAG,"onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
         val videoPath = File(externalCacheDir, "test.mp4").absolutePath
-        val output = File(externalCacheDir, "output.yuv").absolutePath
-        val cmd = "ffmpeg -y -i %s -an -c:v rawvideo -pixel_format yuv420p %s"
+        val output = File(externalCacheDir, "leak.avi").absolutePath
+        val cmd = "ffmpeg -y -i %s -b:v 600k %s"
         val result = String.format(Locale.CHINA, cmd, videoPath, output)
         val strings: Array<String?> = result.split(" ").toTypedArray()
-        FFmpegCommand.runCmd(strings)
-        return super.onStartCommand(intent, flags, startId)
+        FFmpegCommand.runCmd(strings, callback("测试内存抖动", output))
+    }
+
+    private fun callback(msg: String, targetPath: String?): CommonCallBack? {
+        return object : CommonCallBack() {
+            override fun onStart() {
+                Log.d("FFmpegCmd", "onStart")
+            }
+
+            override fun onComplete() {
+                Log.d("FFmpegCmd", "onComplete")
+
+            }
+
+            override fun onCancel() {
+                Log.d("FFmpegCmd", "Cancel")
+            }
+
+            override fun onProgress(progress: Int, pts: Long) {
+                Log.d("FFmpegCmd", progress.toString() + "")
+            }
+
+            override fun onError(errorCode: Int, errorMsg: String?) {
+                Log.d("FFmpegCmd", errorMsg)
+            }
+        }
     }
 }
--- app/src/main/java/com/coder/ffmpegtest/service/FFmpegCommandService2.kt
+++ app/src/main/java/com/coder/ffmpegtest/service/FFmpegCommandService2.kt
@@ -1,5 +1,6 @@
 package com.coder.ffmpegtest.service
 
+import android.app.IntentService
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
@@ -11,18 +12,18 @@ import java.util.*
  * @author: AnJoiner
  * @datetime: 20-6-28
  */
-class FFmpegCommandService2 : Service() {
+class FFmpegCommandService2 : IntentService("") {
     override fun onBind(intent: Intent): IBinder? {
         return null
     }
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+    override fun onHandleIntent(intent: Intent?) {
         val videoPath = File(externalCacheDir, "test.mp4").absolutePath
         val output = File(externalCacheDir, "output3.yuv").absolutePath
         val cmd = "ffmpeg -y -i %s -an -c:v rawvideo -pixel_format yuv420p %s"
         val result = String.format(Locale.CHINA, cmd, videoPath, output)
         val strings: Array<String?> = result.split(" ").toTypedArray()
         FFmpegCommand.runCmd(strings)
-        return super.onStartCommand(intent, flags, startId)
     }
+
 }
