0d436cdaffba757e203e8a816c0d3e9bab485d6b
AnJoiner <Anjoiner@163.com>
2023-11-10 23:25:56 +0800

    :tada: 完成ffmpeg的drawtext和subtitles使用

--- app/src/main/java/com/coder/ffmpegtest/ui/MainActivity.kt
+++ app/src/main/java/com/coder/ffmpegtest/ui/MainActivity.kt
@@ -9,44 +9,43 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.coder.ffmpegtest.R
+import com.coder.ffmpegtest.databinding.ActivityFfmpegFontBinding
+import com.coder.ffmpegtest.databinding.ActivityMainBinding
 import com.coder.ffmpegtest.service.FFmpegCommandService
 
 @SuppressLint("NonConstantResourceId")
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    var mCommandBtn: Button? = null
-    var mInfoBtn: Button? = null
-    var mFormatBtn: Button? = null
-    var mCodecBtn: Button? = null
-    var mAbiText: TextView? = null
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        intiView()
-        initListener()
+        // 通过binding绑定视图
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // 初始化
+        initUI()
+        initClick()
     }
 
-    private fun intiView() {
-        mCommandBtn = findViewById(R.id.btn_command)
-        mInfoBtn = findViewById(R.id.btn_info)
-        mFormatBtn = findViewById(R.id.btn_format)
-        mCodecBtn = findViewById(R.id.btn_codec)
-        mAbiText = findViewById(R.id.tv_abi)
-        mAbiText?.text = String.format("当前使用cpu-abi：%s", Build.CPU_ABI)
+    private fun initUI() {
+        binding.abiText.text = String.format("当前使用cpu-abi：%s", Build.CPU_ABI)
     }
 
-    private fun initListener() {
-        mCommandBtn!!.setOnClickListener(this)
-        mInfoBtn!!.setOnClickListener(this)
-        mFormatBtn!!.setOnClickListener(this)
-        mCodecBtn!!.setOnClickListener(this)
+    private fun initClick() {
+        binding.commandBtn.setOnClickListener(this)
+        binding.infoBtn.setOnClickListener(this)
+        binding.formatBtn.setOnClickListener(this)
+        binding.codecBtn.setOnClickListener(this)
+        binding.fontBtn.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.btn_command -> KFFmpegCommandActivity.start(this)
-            R.id.btn_info -> KFFmpegInfoActivity.start(this)
-            R.id.btn_format -> KFFmppegFormatActivity.start(this)
-            R.id.btn_codec -> KFFmpegCodecActivity.start(this)
+            R.id.command_btn -> KFFmpegCommandActivity.start(this)
+            R.id.info_btn -> KFFmpegInfoActivity.start(this)
+            R.id.format_btn -> KFFmppegFormatActivity.start(this)
+            R.id.codec_btn -> KFFmpegCodecActivity.start(this)
+            R.id.font_btn -> KFFmpegFontActivity.start(this)
         }
     }
 }
